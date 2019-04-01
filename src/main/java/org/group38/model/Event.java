2b766cf2eabc2b508f8c8e9785b1866dda85a376
Martina <martinarebekka@gmail.com>
2019-04-01 15:34:39 +0200

    litt endringer i event

--- src/main/java/org/group38/model/Event.java
+++ src/main/java/org/group38/model/Event.java
@@ -4,27 +4,26 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Event {
-    ArrayList<Ticket> tickets; //kjøpte og ikke kjøpte billetter
+    ArrayList<Ticket> tickets; 
     //Kontaktperson kontaktperson;
     String eventName;
-    //Type, hvordan skal vi definere denne?
+    String type;//velge fra rullegardin
     String participants;
-    //Facility lokale;
-    //program;
+    //Facility facility;
+    //String program?;
     Date date;
     double ticketPrice;
     int maxTickets;
 
     //constructor
-    public Event(String eventName, int maxTickets, String participants, Date date, double ticketPrice){
+    public Event(String eventName, String type, int maxTickets, String participants, Date date, double ticketPrice){
         tickets=new ArrayList<>(maxTickets);
         this.eventName=eventName;
+        this.type=type;
         this.participants=participants;
         this.date=date;
         this.ticketPrice=ticketPrice;
         this.maxTickets=maxTickets;
-
-
     }
     //delete
     //edit
