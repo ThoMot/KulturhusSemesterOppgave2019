4740ecef5681d8b2e311766957584ad1f9e270bd
Martina <martinarebekka@gmail.com>
2019-04-01 15:26:43 +0200

    opprettet konstruktør til event

--- src/main/java/org/group38/model/Event.java
+++ src/main/java/org/group38/model/Event.java
@@ -4,18 +4,28 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Event {
-    ArrayList<Ticket> billetter; //kjøpte og ikke kjøpte billetter
+    ArrayList<Ticket> tickets; //kjøpte og ikke kjøpte billetter
     //Kontaktperson kontaktperson;
-    String name;
+    String eventName;
     //Type, hvordan skal vi definere denne?
     String participants;
     //Facility lokale;
     //program;
     Date date;
-    int ticketPrice;
+    double ticketPrice;
     int maxTickets;
 
     //constructor
+    public Event(String eventName, int maxTickets, String participants, Date date, double ticketPrice){
+        tickets=new ArrayList<>(maxTickets);
+        this.eventName=eventName;
+        this.participants=participants;
+        this.date=date;
+        this.ticketPrice=ticketPrice;
+        this.maxTickets=maxTickets;
+
+
+    }
     //delete
     //edit
     //set
