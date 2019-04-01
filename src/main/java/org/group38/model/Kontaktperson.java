0d9bf2e636cf444be27e496351a065ac04f6b25c
Thora <thora.marie@outlook.com>
2019-04-02 01:22:43 +0200

    Startet opp Kontaktperson
    Må se litt på hva vi skal gjøre med konstruktøren her.

--- src/main/java/org/group38/model/Kontaktperson.java
+++ src/main/java/org/group38/model/Kontaktperson.java
@@ -1,4 +1,23 @@
 package org.group38.model;
 
 public class Kontaktperson {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNr;
+    private String webPage;
+    private String affiliation;
+    private String notes;
+
+    public Kontaktperson(String firstName, String lastName, String email, String phoneNr, String ... x){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+        this.phoneNr=phoneNr;
+        this.webPage=
+                this.affiliation=affiliation;
+        this.notes=notes;
+    }
+
 }
