b07bb1899ddd487610119601c573a85f477306c7
Unknown <kjersti_945@hotmail.com>
2019-04-03 10:56:45 +0200

    Facility endringer

--- src/main/java/org/group38/model/Facility.java
+++ src/main/java/org/group38/model/Facility.java
@@ -1,38 +1,101 @@
 package org.group38.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Facility {
-    String facilityName;
-    String type;
-    int rows;
-    int columns;
-    int totalSeats;
+    private final String facilityName;
+    private String facilityType;
+    private List<Seat> seats = new ArrayList<>();
 
-    //constructor
-    public Facility(String facilityName, String type, int rows, int columns){
-        this.columns=columns;
-        this.rows=rows;
-        this.facilityName=facilityName;
-        this.type=type;
-        this.totalSeats=columns*rows; //burde vi bruke logikk her, eller er det bedre å legge inn egen variabel?
+
+    // Constructor
+
+    public Facility(){
     }
 
-    public int getRows() {
-        return rows;
-    }
+    public Facility(String facilityName, String facilityType, int numRows, int seatsPerRow){
+        this.facilityName = facilityName;
+        this.facilityType = facilityType;
 
-    public int getColumns() {
-        return columns;
-    }
 
-    public int getTotalSeats() {
-        return totalSeats;
+        // Creating seats with rows
+        int lastRow = 'A' + (numRows -1);
+        for(char row = 'A'; row <= lastRow; row++){
+            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++){
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+
     }
 
     public String getFacilityName() {
         return facilityName;
     }
 
-    public String getType() {
-        return type;
+    public String getFacilityType() {
+        return facilityType;
+    }
+
+    // Check if seat exists and reserve seat.
+    public boolean reserveSeat(String seatNumber){
+        Seat requestedSeat = null;
+        for(Seat seat : seats){
+            if(seat.getSeatNumber().equals(seatNumber)){
+                requestedSeat = seat;
+                break;
+            }
+        }
+
+        if(requestedSeat == null){
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+
+        return requestedSeat.reserve();
+    }
+
+    // for testing, skal slettes
+    public void getSeats() {
+        for(Seat seat : seats) {
+            System.out.println(seat.getSeatNumber());
+        }
+    }
+
+    // Subclass of seats
+    public class Seat {
+        private final String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber){
+            this.seatNumber = seatNumber;
+        }
+
+        // Reserve ONE seat if it's not reserved.
+        public boolean reserve() {
+            if(!this.reserved){
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved");
+                return true;
+            } else{
+                return false;
+            }
+        }
+
+        // Cancel reservation if reservation exists.
+        public boolean cancel() {
+            if(this.reserved){
+                this.reserved = false;
+                System.out.println("Reservation of seat " + seatNumber + " cancelled");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
     }
 }
