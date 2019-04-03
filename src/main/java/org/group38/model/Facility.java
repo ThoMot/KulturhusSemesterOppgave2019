3aea6967f30492cdfb4628e47c096356a48297f1
Martina <martinarebekka@gmail.com>
2019-04-03 14:32:36 +0200

    Lagt oversikt over hva som skal gjøres i facility

--- src/main/java/org/group38/model/Facility.java
+++ src/main/java/org/group38/model/Facility.java
@@ -1,3 +1,6 @@
+//fjerne alle metoder for billett og reservasjon, men se først om det kan brukes i den andre reservasjonen
+//lag tostring metode
+//lage setmetoder som også sjekker at det blir gitt riktig type input
 package org.group38.model;
 
 import java.util.ArrayList;
