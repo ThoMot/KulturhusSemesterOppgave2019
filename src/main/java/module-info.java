6e043253ce4d65570a526769ce88eb8d67ede0e2
Thora <thora.marie@outlook.com>
2019-04-10 17:15:43 +0200

    Nå funker det

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,7 @@ module hellofx {
 
     opens org.group38 to javafx.fxml;
     exports org.group38;
+
+    opens org.group38.model.Controllers to javafx.fxml;
+    exports org.group38.model.Controllers;
 }
--- src/main/java/org/group38/MainApp.java
+++ src/main/java/org/group38/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("frontPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("frontpage.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
