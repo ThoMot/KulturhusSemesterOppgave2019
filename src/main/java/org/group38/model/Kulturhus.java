0f8de055d0a3745a7787d9ec426dc49c83d05e49
Unknown <martinarebekka@gmail.com>
2019-04-12 13:55:44 +0200

    minor fixes

--- src/main/java/org/group38/model/Kulturhus.java
+++ src/main/java/org/group38/model/Kulturhus.java
@@ -1,12 +1,30 @@
 //slette arrangementer etter at datoen har vært?
-//egen metode som kjører finnbillett metoden, som returnerer feilmelding dersom finnbillett returnerer null
 package org.group38.model;
 
+import org.group38.model.ContactPerson.ContactInfo;
+import org.group38.model.ContactPerson.ContactPerson;
 import org.group38.model.Event.Event;
+import org.group38.model.Event.EventInfo;
+import org.group38.model.Event.EventNumberedSeating;
+import org.group38.model.Event.Performer;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kulturhus {
     private ArrayList<Event> events;
+
+
+    //kun for testing
+    Facility facility=new Facility("Sal1","kino", 10,10);
+    Calendar d= new GregorianCalendar(2019, 10,10, 22,00);
+    ContactPerson contactPerson = new ContactPerson("Martina", "Førre", new ContactInfo("martina@gmail.com","11223344"));
+    EventInfo eventInfo = new EventInfo("Max Manus", "film",d);
+    ArrayList<Performer> performers= new ArrayList <Performer>();
+
+    EventNumberedSeating maxManus=new EventNumberedSeating(contactPerson, facility, performers, 100, eventInfo);
+
+
 
 }
