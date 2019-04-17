7e053c9a5b7b3c5f7b7842709cdc6d25833c3c98
Thora <thora.marie@outlook.com>
2019-04-17 16:36:29 +0200

    Added functionality to menu on addTicket

--- src/main/java/org/group38/kulturhus/controllers/AddTicketController.java
+++ src/main/java/org/group38/kulturhus/controllers/AddTicketController.java
@@ -1,14 +1,36 @@
 package org.group38.kulturhus.controllers;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import org.group38.kulturhus.sceneHandling.SceneManager;
+import org.group38.kulturhus.sceneHandling.SceneName;
+
+import java.io.IOException;
 
 //KJERSTI JOBBER HER
 public class AddTicketController implements MainController{
 
     @FXML
-    private AnchorPane anchorAddTicket;
+    private void goToAddEvent(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.ADDEVENT);
+    }
+
+    @FXML
+    private void goToShowTicket(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.SHOWTICKET);
+    }
+
+    @FXML
+    private void goToShowEvent(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.ADDEVENT);
+    }
+
+    @FXML
+    private void goToShowVenue(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.SHOWVENUE);
+    }
 
 
 
