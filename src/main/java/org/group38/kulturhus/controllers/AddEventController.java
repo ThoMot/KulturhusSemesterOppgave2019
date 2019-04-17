d424a48cecdfc02adb5e0de0049630b21c2bfe9c
Thora <thora.marie@outlook.com>
2019-04-17 12:14:26 +0200

    Started adding fx pages and coupling them

--- src/main/java/org/group38/kulturhus/controllers/AddEventController.java
+++ src/main/java/org/group38/kulturhus/controllers/AddEventController.java
@@ -1,6 +1,37 @@
 package org.group38.kulturhus.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.group38.kulturhus.sceneHandling.SceneManager;
+import org.group38.kulturhus.sceneHandling.SceneName;
+
+import java.io.IOException;
+
 public class AddEventController implements MainController {
+
+    public AddEventController() {
+    }
+
+    @FXML
+    private void goToAddTicket(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.ADDTICKET);
+    }
+
+    @FXML
+    private void goToShowTicket(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.SHOWTICKET);
+    }
+
+    @FXML
+    private void goToShowEvent(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.ADDEVENT);
+    }
+
+    @FXML
+    private void goToShowVenue(ActionEvent event) throws IOException {
+        SceneManager.navigate(SceneName.SHOWVENUE);
+    }
+
 
 
     @Override
