8e922de0a0c2db870dd54fe21b9ace26a9c6dcac
Unknown <kjersti_945@hotmail.com>
2019-04-24 16:39:42 +0200

    Jobj fil start

--- src/main/java/org/group38/kulturhus/model/SaveLoad/SaveJobj.java
+++ src/main/java/org/group38/kulturhus/model/SaveLoad/SaveJobj.java
@@ -2,25 +2,31 @@ package org.group38.kulturhus.model.SaveLoad;
 
 import org.group38.kulturhus.model.Event.EventNumberedSeating;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-public class SaveJobj implements SaveDataInterface{
+
+public class SaveJobj implements org.group38.kulturhus.model.SaveLoad.SaveDataInterface {
 
     @Override
     public boolean writeEvent(EventNumberedSeating newSubstituteUser) {
         try {
-            FileOutputStream fos = new FileOutputStream("newUser.jobj");
+            FileOutputStream fos = new FileOutputStream("ticket.jobj");
             ObjectOutputStream out = new ObjectOutputStream(fos);
-            out.writeObject(newSubstituteUser);
+            out.writeObject(Object.class);
+            out.close();
             return true;
 
         } catch(IOException e) {
             e.printStackTrace(); // This should not happen, so we print debug information here.
             return false;
         }
+
     }
+
+
 }
 
 
