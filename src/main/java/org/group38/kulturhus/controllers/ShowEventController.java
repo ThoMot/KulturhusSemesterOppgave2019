81ecda8f9e1f5892669763e7b5b97856004ed3aa
Unknown <martinarebekka@gmail.com>
2019-04-29 13:11:15 +0200

    kommentert showEvent

--- src/main/java/org/group38/kulturhus/controllers/ShowEventController.java
+++ src/main/java/org/group38/kulturhus/controllers/ShowEventController.java
@@ -7,7 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-import javafx.scene.layout.VBox;
 import org.group38.kulturhus.sceneHandling.SceneManager;
 import org.group38.kulturhus.sceneHandling.SceneName;
 import org.group38.kulturhus.model.Event.Event;
@@ -16,54 +15,21 @@ import static org.group38.kulturhus.ErrorBoxes.errorNoMarkedEvent;
 import static org.group38.kulturhus.model.Kulturhus.*;
 
 public class ShowEventController implements MainController{
+    //data field
     private ObservableList<Event> observableList;
     private static Event selectedEvent;
 
     @FXML private TableView<Event> eventsView;
     @FXML private TableColumn<Event,String> eventDateColumn, eventTimeColumn, eventNameColumn, eventFacilityColumn;
+
+    /*
+    Methods for opening to different scenes, and setting the selected event if needed in the next scene.
+    it also shows an errormessage in an alert if there is no selected event
+     */
     @FXML
-    private void goToAddEvent(ActionEvent event){
-       SceneManager.navigate(SceneName.ADDEVENT);
-    }
+    private void goToAddEvent(ActionEvent event){ SceneManager.navigate(SceneName.ADDEVENT); }
     @FXML
-    private void goToShowVenue(ActionEvent event){
-        SceneManager.navigate(SceneName.SHOWVENUE);
-    }
-    public void initialize(){
-        initCols();
-        loadData();
-        setSelectedEvent(null);
-
-    }
-    private void initCols(){
-        eventNameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEventInfo().getEventName()));
-        eventTimeColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEventInfo().getTime().toString()));
-        eventDateColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEventInfo().getDate().toString()));
-        eventFacilityColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getFacility().getFacilityName()));
-    }
-
-    private void loadData(){
-        observableList = FXCollections.observableList(getEvents());
-        eventsView.setItems(observableList);
-    }
-    public void deleteRow(ActionEvent event){
-        if(eventsView.getSelectionModel().getSelectedItem()==null){
-            errorNoMarkedEvent();
-        }
-        else{
-            Alert mb = new Alert(Alert.AlertType.CONFIRMATION);
-            mb.setTitle("Bekreft");
-            mb.setHeaderText("Du har trykket slett på "+ eventsView.getSelectionModel().getSelectedItem().getEventInfo().getEventName());
-            mb.setContentText("Ønsker du virkerlig å slette dette arrangementet?");
-            mb.showAndWait().ifPresent(response -> {
-                if(response==ButtonType.OK){
-                    observableList.remove(eventsView.getSelectionModel().getSelectedItem());
-                }
-            });
-        }
-    }
-
-    //metode som bytter scene til visinfo og tar med seg eventet som er trykket på
+    private void goToShowVenue(ActionEvent event){ SceneManager.navigate(SceneName.SHOWVENUE); }
     public void goToVisInfo(ActionEvent event){
         if(eventsView.getSelectionModel().getSelectedItem()==null){
             errorNoMarkedEvent();
@@ -92,10 +58,56 @@ public class ShowEventController implements MainController{
         }
     }
 
+
+    /*
+    The initialize method runs when the scene is opened. This method runs the
+    initCols method and the loadData method. It also sets the selectedEvent to null
+    for not bringing an old selected item to the next scene.
+     */
+    public void initialize(){
+        initCols();
+        loadData();
+        setSelectedEvent(null);
+
+    }
+    //** initCols method is deciding what the table columns shall contain
+    private void initCols(){
+        eventNameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEventInfo().getEventName()));
+        eventTimeColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEventInfo().getTime().toString()));
+        eventDateColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEventInfo().getDate().toString()));
+        eventFacilityColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getFacility().getFacilityName()));
+    }
+
+    //**The loadData method adds all the events from the list in Kulturhus to the tableview
+    private void loadData(){
+        observableList = FXCollections.observableList(getEvents());
+        eventsView.setItems(observableList);
+    }
+    /*
+    The deleteRow method checks if an event is selected, or else shows an errorMessage.
+    If a field is selected, shows a confirmation alert, and then deletes the event if the
+    user presses the ok button.
+     */
+    public void deleteRow(ActionEvent event){
+        if(eventsView.getSelectionModel().getSelectedItem()==null){
+            errorNoMarkedEvent();
+        }
+        else{
+            Alert mb = new Alert(Alert.AlertType.CONFIRMATION);
+            mb.setTitle("Bekreft");
+            mb.setHeaderText("Du har trykket slett på "+ eventsView.getSelectionModel().getSelectedItem().getEventInfo().getEventName());
+            mb.setContentText("Ønsker du virkerlig å slette dette arrangementet?");
+            mb.showAndWait().ifPresent(response -> {
+                if(response==ButtonType.OK){
+                    observableList.remove(eventsView.getSelectionModel().getSelectedItem());
+                }
+            });
+        }
+    }
+    //**setter and getter for the selectedEvent
     public static void setSelectedEvent(Event selectedEvent) {
         ShowEventController.selectedEvent = selectedEvent;
     }
-
     public static Event getSelectedEvent() {
         return selectedEvent;
     }
