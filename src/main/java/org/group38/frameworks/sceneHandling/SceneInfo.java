3e58944672e7342bc08651f80788dfc20a546118
Kjersti <kjersti_945@hotmail.com>
2019-05-03 13:08:36 +0200

    Kommentar på sceneHandler

--- src/main/java/org/group38/frameworks/sceneHandling/SceneInfo.java
+++ src/main/java/org/group38/frameworks/sceneHandling/SceneInfo.java
@@ -6,23 +6,21 @@ public class SceneInfo {
     private final String sceneTitle;
     private final String viewpath;
 
-
-
+    /** SceneInfo method sets the title and the path to the new scenes */
     public SceneInfo(String title, String viewpath){
         this.sceneTitle = title;
         this.viewpath = Strings.requireNonNullAndNotEmpty(viewpath);
     }
 
+    /** getter for sceneTitle */
     public String getSceneTitle() {
         return sceneTitle;
     }
 
+    /** getter for viewPath*/
     public String getViewpath() {
         return viewpath;
     }
-
-
-
 
     @Override
     public String toString() {
